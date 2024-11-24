3dd753568f726b778c8ed8eb4601d1f515156a6b
Promise-Digitals <promiseedeh025@gmail.com>
2024-11-24 20:33:53 +0100

    remove album API created

--- music-backend/src/controllers/albumController.js
+++ music-backend/src/controllers/albumController.js
@@ -57,7 +57,21 @@ const listAlbum = async (req, res) => {
 
 
 const removeAlbum = async (req, res) => {
-    
+    try {
+        
+        await albumModel.findByIdAndDelete(req.body.id);
+
+        res.json({
+            success: true,
+            message: "Album removed"
+        })
+
+    } catch (error) {
+        res.json({
+            success: false
+        })
+
+    }
 }
 
 
